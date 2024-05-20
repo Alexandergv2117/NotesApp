94740da8e1dcf6b3bf17735eef63ee5f6d98d290
Alexander Garcia <alexandergv2117@gmail.com>
2024-05-19 23:26:52 -0500

    chore(frontend): config tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='text-3xl'>
+      <h1 className='font-bold text-xs'>Notes APP</h1>
+    </main>
+  )
+}
+
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const root = createRoot(document.getElementById('app')!);
-
-root.render(<h1>Hello, world!</h1>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
