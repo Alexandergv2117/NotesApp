b3627f2405c49f8d00335901de784cbf39e45817
Alexander Garcia <alexandergv2117@gmail.com>
2024-05-20 23:46:56 -0500

    chore(frontend): config project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 
 function App() {
+
   return (
-    <main className='text-3xl'>
-      <h1 className='font-bold text-xs'>Notes APP</h1>
+    <main className='flex justify-center'>
+      <div>
+        <h1 className='font-bold text-4xl'>Notes APP</h1>
+      </div>
     </main>
   )
 }
