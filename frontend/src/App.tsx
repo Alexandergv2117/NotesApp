2989f8e4da38e97a6df960f80f789765a7060af5
Alexander Garcia <74172014+Alexandergv2117@users.noreply.github.com>
2024-05-22 00:01:33 -0500

    Config frontend project (#2)
    * chore(frontend): config project
    
    * chore(frontend): config tailwind
    
    * chore(frontend): config project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='flex justify-center'>
+      <div>
+        <h1 className='font-bold text-4xl'>Notes APP</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
